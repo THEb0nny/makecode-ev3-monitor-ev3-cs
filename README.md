58729dfc85a636bd81df9dbcafb563224d725ef6
Dmitriy Antipov <aston_bonny@mail.ru>
2026-05-23 00:38:07 +0300

    Первоначальные файлы для проекта MakeCode

---
+++ README.md
@@ -0,0 +1,24 @@
+
+> Открыть эту страницу в [https://theb0nny.github.io/makecode-ev3-monitor_ev3_cs/](https://theb0nny.github.io/makecode-ev3-monitor_ev3_cs/)
+
+## Использовать это расширение
+
+Этот репозиторий может быть добавлен в MakeCode как **расширение**.
+
+* открыть [/pxt-ev3/beta/](/pxt-ev3/beta/)
+* нажмите на **Новый проект**
+* нажмите **Расширения** в меню-шестерёнке
+* найдите **https://github.com/theb0nny/makecode-ev3-monitor_ev3_cs** и импортируйте
+
+## Править этот проект
+
+Редактировать этот репозиторий в MakeCode.
+
+* открыть [/pxt-ev3/beta/](/pxt-ev3/beta/)
+* нажмите на **Импорт**, затем **Импорт URL**
+* вставьте **https://github.com/theb0nny/makecode-ev3-monitor_ev3_cs** и нажмите Импорт
+
+#### Метаданные (используются для поиска, рендеринга)
+
+* for PXT/ev3
+<script src="https://makecode.com/gh-pages-embed.js"></script><script>makeCodeRender("{{ site.makecode.home_url }}", "{{ site.github.owner_name }}/{{ site.github.repository_name }}");</script>
---
+++ main.ts
@@ -0,0 +1,83 @@
+const lineSensors: sensors.ColorSensor[] = [sensors.color1, sensors.color2, sensors.color3, sensors.color4]; // Массив всех портов датчиков цвета ev3
+
+const fileName = "ref_raw_ev3_cs.txt"; // Имя временного файла записи медианных значений
+
+function Main() {
+    let state = State.ShowValues; // Состояние конечного автомата
+
+    let refRaw: number[] = [0, 0, 0, 0]; // Массив для хранения сырых значений отражения с датчика
+
+    let whiteRefRawValues: number[][] = [[], [], [], []]; // Массив для хранения сырых значений на белом
+    let blackRefRawValues: number[][] = [[], [], [], []]; // Массив для хранения сырых значений на чёрном
+
+    let whiteRefRawMedianValues: number[] = [0, 0, 0, 0]; // Массивы хранения медианных значений для белого
+    let blackRefRawMedianValues: number[] = [0, 0, 0, 0]; // Массивы хранения медианных значений для чёрного
+
+    while (true) {
+        for (let i = 0; i < 4; i++) { // Считываем сырые значения отражения с датчика
+            refRaw[i] = lineSensors[i].light(LightIntensityMode.ReflectedRaw);
+        }
+
+        let pauseMs = 10; // Пауза для цикла, установить значение по умолчанию
+
+        if (state == State.ShowValues && brick.buttonEnter.isPressed()) {
+            state = State.Waiting;
+        } else if (state == State.Waiting && !brick.buttonEnter.isPressed()) {
+            state = State.Search;
+        } else if (state == State.Search && brick.buttonUp.isPressed()) {
+            state = State.SaveWhiteRefRaw;
+            for (let i = 0; i < 4; i++) whiteRefRawValues[i].push(refRaw[i]);
+            // console.log(colorSensorRefRaw.join(', '));
+        } else if (state == State.Search && brick.buttonDown.isPressed()) {
+            state = State.SaveBlackRefRaw;
+            for (let i = 0; i < 4; i++) blackRefRawValues[i].push(refRaw[i]);
+            // console.log(colorSensorRefRaw.join(', '));
+        } else if ((state == State.SaveWhiteRefRaw || state == State.SaveBlackRefRaw) && !brick.buttonUp.isPressed() && !brick.buttonDown.isPressed()) {
+            state = State.Search;
+        } else if (state == State.Search && brick.buttonEnter.isPressed()) {
+            state = State.CalculateMedian;
+            for (let i = 0; i < 4; i++) {
+                whiteRefRawMedianValues[i] = calculateMedian(whiteRefRawValues[i]);
+                blackRefRawMedianValues[i] = calculateMedian(blackRefRawValues[i]);
+            }
+            storage.temporary.remove(fileName); // Удалить файл с таким же именем
+            storage.temporary.limit(fileName, 0); // Установить лимит размера файла
+            storage.temporary.appendLine(fileName, "White medians:");
+            storage.temporary.appendLine(fileName, whiteRefRawMedianValues.join(', '));
+            storage.temporary.appendLine(fileName, "Black medians:");
+            storage.temporary.appendLine(fileName, blackRefRawMedianValues.join(', '));
+        } else if (state == State.CalculateMedian) {
+            state = State.CalculationCompleted;
+        }
+
+        brick.clearScreen(); // Выводим на экран
+        for (let i = 0; i < 4; i++) brick.showValue("refRawPort" + (i + 1), refRaw[i], i + 1);
+        if (state == State.ShowValues) {
+            brick.printString("Press ENTER to search", 11);
+            brick.printString("median values", 12);
+        } else if (state == State.Search) {
+            brick.printString("Press UP to save WHITE", 6);
+            brick.printString("Press DOWN to save BLACK", 7);
+            brick.printString("Press ENTER to calculate", 9);
+            brick.printString("median values", 10);
+        } else if (state == State.SaveWhiteRefRaw) {
+            brick.printString("Saved to White!", 6);
+            pauseMs = 500;
+        } else if (state == State.SaveBlackRefRaw) {
+            brick.printString("Saved to Black!", 6);
+            pauseMs = 500;
+        } else if (state == State.CalculateMedian) {
+            brick.printString("Processing...", 9);
+            pauseMs = 300;
+        } else if (state == State.CalculationCompleted) {
+            brick.printString("WHITE medians: ", 7);
+            brick.printString(whiteRefRawMedianValues.join(', '), 8);
+            brick.printString("BLACK medians: ", 10);
+            brick.printString(blackRefRawMedianValues.join(', '), 11);
+        }
+
+        loops.pause(pauseMs); // Небольшая задержка для стабильности
+    }
+}
+
+Main();
