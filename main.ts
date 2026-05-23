2fa68bf3b2fd74ed6031aeb0abb4f30058ebf028
Dmitriy Antipov <aston_bonny@mail.ru>
2026-05-23 13:46:07 +0300

    Обновить main.ts, custom.ts

--- main.ts
+++ main.ts
@@ -1,83 +1,151 @@
-const lineSensors: sensors.ColorSensor[] = [sensors.color1, sensors.color2, sensors.color3, sensors.color4]; // Массив всех портов датчиков цвета ev3
+const lineSensors: sensors.ColorSensor[] = [
+    sensors.color1,
+    sensors.color2,
+    sensors.color3,
+    sensors.color4
+]; // Массив всех портов датчиков цвета ev3
 
-const fileName = "ev3_cs_ref_raw.rtf"; // Имя временного файла записи медианных значений
+const fileName = "nxt_ls_ref_raw.rtf"; // Имя временного файла записи медианных значений
 
-function Main() {
-    let state = State.ShowValues; // Состояние конечного автомата
+let state = State.ShowValues; // Состояние конечного автомата
 
-    let refRaw: number[] = [0, 0, 0, 0]; // Массив для хранения сырых значений отражения с датчика
+const maxSamples = 50; // Максимальное количество семплов при локальной записи
 
-    let whiteRefRawValues: number[][] = [[], [], [], []]; // Массив для хранения сырых значений на белом
-    let blackRefRawValues: number[][] = [[], [], [], []]; // Массив для хранения сырых значений на чёрном
+let refRaw: number[] = [0, 0, 0, 0]; // Массив для хранения сырых значений отражения с датчика
 
-    let whiteRefRawMedianValues: number[] = [0, 0, 0, 0]; // Массивы хранения медианных значений для белого
-    let blackRefRawMedianValues: number[] = [0, 0, 0, 0]; // Массивы хранения медианных значений для чёрного
+// Первичная калибровка
+let whiteRefRaw: number[] = [0, 0, 0, 0];
+let blackRefRaw: number[] = [0, 0, 0, 0];
 
-    while (true) {
-        for (let i = 0; i < 4; i++) { // Считываем сырые значения отражения с датчика
-            refRaw[i] = lineSensors[i].light(LightIntensityMode.ReflectedRaw);
-        }
+let threshold: number[] = [0, 0, 0, 0];
 
-        let pauseMs = 10; // Пауза для цикла, установить значение по умолчанию
+let whiteSamples: number[][] = [[], [], [], []];
+let blackSamples: number[][] = [[], [], [], []];
 
-        if (state == State.ShowValues && brick.buttonEnter.isPressed()) {
-            state = State.Waiting;
-        } else if (state == State.Waiting && !brick.buttonEnter.isPressed()) {
-            state = State.Search;
-        } else if (state == State.Search && brick.buttonUp.isPressed()) {
-            state = State.SaveWhiteRefRaw;
-            for (let i = 0; i < 4; i++) whiteRefRawValues[i].push(refRaw[i]);
-            // console.log(colorSensorRefRaw.join(', '));
-        } else if (state == State.Search && brick.buttonDown.isPressed()) {
-            state = State.SaveBlackRefRaw;
-            for (let i = 0; i < 4; i++) blackRefRawValues[i].push(refRaw[i]);
-            // console.log(colorSensorRefRaw.join(', '));
-        } else if ((state == State.SaveWhiteRefRaw || state == State.SaveBlackRefRaw) && !brick.buttonUp.isPressed() && !brick.buttonDown.isPressed()) {
-            state = State.Search;
-        } else if (state == State.Search && brick.buttonEnter.isPressed()) {
-            state = State.CalculateMedian;
-            for (let i = 0; i < 4; i++) {
-                whiteRefRawMedianValues[i] = median(whiteRefRawValues[i]);
-                blackRefRawMedianValues[i] = median(blackRefRawValues[i]);
-            }
-            storage.temporary.remove(fileName); // Удалить файл с таким же именем
-            storage.temporary.limit(fileName, 0); // Установить лимит размера файла
-            storage.temporary.appendLine(fileName, "White medians:");
-            storage.temporary.appendLine(fileName, whiteRefRawMedianValues.join(', '));
-            storage.temporary.appendLine(fileName, "Black medians:");
-            storage.temporary.appendLine(fileName, blackRefRawMedianValues.join(', '));
-        } else if (state == State.CalculateMedian) {
-            state = State.CalculationCompleted;
-        }
+let whiteMedian: number[] = [0, 0, 0, 0];
+let blackMedian: number[] = [0, 0, 0, 0];
 
-        brick.clearScreen(); // Выводим на экран
-        for (let i = 0; i < 4; i++) brick.showValue("refRawPort" + (i + 1), refRaw[i], i + 1);
-        if (state == State.ShowValues) {
-            brick.printString("Press ENTER to search", 11);
-            brick.printString("median values", 12);
-        } else if (state == State.Search) {
-            brick.printString("Press UP to save WHITE", 6);
-            brick.printString("Press DOWN to save BLACK", 7);
-            brick.printString("Press ENTER to calculate", 9);
-            brick.printString("median values", 10);
-        } else if (state == State.SaveWhiteRefRaw) {
-            brick.printString("Saved to White!", 6);
-            pauseMs = 300;
-        } else if (state == State.SaveBlackRefRaw) {
-            brick.printString("Saved to Black!", 6);
-            pauseMs = 300;
-        } else if (state == State.CalculateMedian) {
-            brick.printString("Processing...", 9);
-            pauseMs = 300;
-        } else if (state == State.CalculationCompleted) {
-            brick.printString("WHITE medians: ", 7);
-            brick.printString(whiteRefRawMedianValues.join(', '), 8);
-            brick.printString("BLACK medians: ", 10);
-            brick.printString(blackRefRawMedianValues.join(', '), 11);
-        }
+let allWhiteMedians: number[][] = [[], [], [], []];
+let allBlackMedians: number[][] = [[], [], [], []];
 
-        loops.pause(pauseMs); // Небольшая задержка для стабильности
+let finalWhite: number[] = [0, 0, 0, 0];
+let finalBlack: number[] = [0, 0, 0, 0];
+
+let calibrationCount = 0;
+
+let uiUpdateTime = control.millis();
+
+forever(function () {
+    for (let i = 0; i < 4; i++) { // Считываем сырые значения отражения с датчика
+        refRaw[i] = lineSensors[i].light(LightIntensityMode.ReflectedRaw);
     }
-}
 
-Main();
+    if (state == State.ShowValues && brick.buttonEnter.wasPressed()) {
+        state = State.WaitWhite;
+    } else if (state == State.WaitWhite && brick.buttonUp.wasPressed()) {
+        for (let i = 0; i < 4; i++) {
+            whiteRefRaw[i] = refRaw[i];
+        }
+        state = State.WaitBlack;
+    } else if (state == State.WaitBlack && brick.buttonDown.wasPressed()) {
+        for (let i = 0; i < 4; i++) {
+            blackRefRaw[i] = refRaw[i];
+            threshold[i] = Math.round((whiteRefRaw[i] + blackRefRaw[i]) / 2);
+        }
+        state = State.WaitCalibrationStart;
+    } else if (state == State.WaitCalibrationStart && brick.buttonEnter.wasPressed()) {
+        whiteSamples = [[], [], [], []];
+        blackSamples = [[], [], [], []];
+        state = State.Calibration;
+    } else if (state == State.Calibration) {
+        if (brick.buttonEnter.wasPressed()) {
+            for (let i = 0; i < 4; i++) {
+                // Добавляем остатки WHITE
+                if (whiteSamples[i].length) {
+                    whiteMedian[i] = median(whiteSamples[i]);
+                    allWhiteMedians[i].push(whiteMedian[i]);
+                    whiteSamples[i] = [];
+                }
+                // Добавляем остатки BLACK
+                if (blackSamples[i].length) {
+                    blackMedian[i] = median(blackSamples[i]);
+                    allBlackMedians[i].push(blackMedian[i]);
+                    blackSamples[i] = [];
+                }
+            }
+            calibrationCount++;
+            state = State.Completed;
+        } else {
+            for (let i = 0; i < 4; i++) {
+                // WHITE
+                if (refRaw[i] < threshold[i]) {
+                    whiteSamples[i].push(refRaw[i]);
+                    // Накопили пакет
+                    if (whiteSamples[i].length >= maxSamples) {
+                        allWhiteMedians[i].push(median(whiteSamples[i]));
+                        whiteSamples[i] = [];
+                    }
+                } else {
+                    // BLACK
+                    blackSamples[i].push(refRaw[i]);
+                    // Накопили пакет
+                    if (blackSamples[i].length >= maxSamples) {
+                        allBlackMedians[i].push(median(blackSamples[i]));
+                        blackSamples[i] = [];
+                    }
+                }
+            }
+        }
+    }
+
+    if (state == State.Completed && brick.buttonEnter.wasPressed()) {
+        for (let i = 0; i < 4; i++) {
+            finalWhite[i] = median(allWhiteMedians[i]);
+            finalBlack[i] = median(allBlackMedians[i]);
+        }
+        state = State.FinalResults;
+    } else if (state == State.Completed && brick.buttonUp.wasPressed()) {
+        state = State.WaitCalibrationStart;
+    }
+
+    // UI
+    if (control.millis() - uiUpdateTime > 50) {
+        uiUpdateTime = control.millis();
+        brick.clearScreen();
+        for (let i = 0; i < 4; i++) {
+            brick.showValue("P" + (i + 1), refRaw[i], i + 1);
+        }
+
+        if (state == State.ShowValues) {
+            brick.printString("Нажмите ENTER", 6);
+            brick.printString("для старта калибровки", 7);
+        } else if (state == State.WaitWhite) {
+            brick.printString("Поставьте датчики на БЕЛОЕ", 6);
+            brick.printString("Нажмите ВВЕРХ", 8);
+        } else if (state == State.WaitBlack) {
+            brick.printString("Поставьте датчики на ЧЁРНОЕ", 6);
+            brick.printString("Нажмите ВНИЗ", 8);
+        } else if (state == State.WaitCalibrationStart) {
+            brick.printString("Готово к калибровке", 6);
+            brick.printString("Нажмите ENTER", 7);
+        } else if (state == State.Calibration) {
+            brick.printString("КАЛИБРОВКА", 6);
+            brick.printString("Водите робота по линии", 8);
+            brick.printString("Нажмите ENTER для завершения", 9);
+        } else if (state == State.Completed) {
+            brick.printString(`Проход ${calibrationCount} ЗАВЕРШЁН`, 6);
+            brick.printString("ВВЕРХ для нового прохода", 8);
+            brick.printString("ENTER для завершения калиб-ки", 9);
+            brick.printString("Б: " + whiteMedian.join(", "), 11);
+            brick.printString("Ч: " + blackMedian.join(", "), 12);
+        } else if (state == State.FinalResults) {
+            brick.printString("Калибровка ЗАВЕРШЕНА", 6);
+            brick.printString("Итоговые значения", 8);
+            brick.printString("за все проходы", 9);
+            brick.printString("Б: " + finalWhite.join(", "), 11);
+            brick.printString("Ч: " + finalBlack.join(", "), 12);
+        }
+    }
+
+    loops.pause(1);
+});
